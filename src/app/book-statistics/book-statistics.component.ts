ddaf15c0b9d0bdc65212d6ff2706107a7fef9894
Anatolii <anatolii.kryvda@gmail.com>
2018-07-16 00:38:23 +0300

    Statistics expand details all lines

--- src/app/book-statistics/book-statistics.component.ts
+++ src/app/book-statistics/book-statistics.component.ts
@@ -14,11 +14,12 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
     trigger('wordsExpand', [
       state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
       state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      // transition('expanded <=> collapsed', animate('5s cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
 })
 export class BookStatisticsComponent implements OnInit {
+  // mainColumns: string[] = ['word', 'translation', 'occurrence'];
   dataSource: MatTableDataSource<string[]>;
   pageSizeOptions: number[];
   expandedWords: string[];
