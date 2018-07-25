b9467dda0fc80aa36ef02eeef16a1761b4e7509d
Anatolii Kryvda <anatolii.kryvda@automationanywhere.com>
2018-07-25 13:46:45 +0300

    Stub backend calls for production build

--- src/app/backend-request-interceptor.ts
+++ src/app/backend-request-interceptor.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
 import {AuthService} from './services/auth.service';
+import {environment} from '../environments/environment';
 
 @Injectable()
 export class BackendRequestInterceptor implements HttpInterceptor {
@@ -10,6 +11,10 @@ export class BackendRequestInterceptor implements HttpInterceptor {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (environment.production) {
+      // Some services expect array, it will not break those ones that expect an object
+      return of(new HttpResponse({body: []}));
+    }
     const url = 'http://localhost:8080';
     req = req.clone({
       url: url + req.url,
